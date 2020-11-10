d879160246fb1865aa01cff2069cad4e201a8404
Alex Grogan <agrogan097@gmail.com>
2020-11-10 13:08:47 +0000

    Updated Code

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from kafka import KafkaProducer
 def main():
 
     while True:
+        # Load ADSB data
         all_aircraft_states = stream_adsb.generate_OpenSky_data()
-        try:
-            producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092')
-            producer.send('sample', all_aircraft_states)
-            producer.send('sample', key=b'message-two', value=b'Message Sent')
-            print("Success")
-        except:
-            print("Failure")
+        # Get the icao24 ID of the first flight
+        flight_id = all_aircraft_states[0].icao24
+        print(flight_id)
+        producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092')
+        producer.send('sample', flight_id)
+        # producer.send('sample', key=b'message-two', value=b'Message Sent')
+        # print("Success")
 
     return None
 
